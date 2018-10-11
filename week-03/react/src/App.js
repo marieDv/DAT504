85d2afcd23f5aebfd4fec4ea03a443655e54e96a
marie <dvorzak.marie@gmx.at>
2018-10-11 12:00:50 +0100

    asdsd

--- week-03/react/src/App.js
+++ week-03/react/src/App.js
@@ -2,9 +2,41 @@ import React, { Component } from 'react';
 import  Form  from './validation.js';
 
 class App extends Component {
-  render () {
-    return <Form></Form>
-  }
-}
 
+  componentDidMount(){
+    this.fetchData();
+  }
+
+  onChange = (event) => {
+    fetch('', {
+      method:'POST',
+      body: JSON.stringify(),
+      name: 'Marie',
+    }).then(response => {
+      console.log(response);
+    }))
+  }
+  fetchData = () => {
+    fetch("localhost:3000/adressbook")
+    .then(response => response.text())
+    .then(response => {
+      console.log(response)
+    })
+  }
+  render () {
+    return(
+    <div>
+    <button onClick={this.fetchData}></button>
+     <PeopleList/>
+     </div>
+)}
+}
+const PeopleList = (props) => {
+  const listItems = props.people.map(person => <li>{person.name}</li>)
+  return (
+    <ul>
+    {listItems}
+    </ul>
+  )
+}
 export default App;
